567d802e254369aacaaccd8e1f896232a4ea0b02
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2016-10-29 21:03:43 +0200

    Add gcc config file to source control.

---
+++ Config-gcc.mk
@@ -0,0 +1,11 @@
+RASPPI = 1
+PREFIX = 
+
+CC	= "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-gcc.exe"
+CPP	= "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-c++.exe"
+AS	= "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-gcc.exe"
+LD	= "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-ld.exe"
+AR	= "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-ar.exe"
+OBJDUMP = "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-objdump.exe"
+CPPFILT = "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-c++filt.exe"
+OBJCOPY = "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-objcopy.exe"
