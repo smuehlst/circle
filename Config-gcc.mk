ddd70be4bc547abefc260d0339e89dbcce5df8b4
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2017-01-20 21:13:28 +0100

    Update with current gcc path

--- Config-gcc.mk
+++ Config-gcc.mk
@@ -1,11 +1,11 @@
 RASPPI = 1
 PREFIX = 
 
-CC	= "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-gcc.exe"
-CPP	= "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-c++.exe"
-AS	= "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-gcc.exe"
-LD	= "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-ld.exe"
-AR	= "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-ar.exe"
-OBJDUMP = "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-objdump.exe"
-CPPFILT = "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-c++filt.exe"
-OBJCOPY = "C:\Program Files (x86)\GNU Tools ARM Embedded\5.4 2016q2\bin\arm-none-eabi-objcopy.exe"
+CC	= "E:\Users\stm\local\gcc-arm-none-eabi-5_4-2016q3-20160926-win32\bin\arm-none-eabi-gcc.exe"
+CPP	= "E:\Users\stm\local\gcc-arm-none-eabi-5_4-2016q3-20160926-win32\bin\arm-none-eabi-c++.exe"
+AS	= "E:\Users\stm\local\gcc-arm-none-eabi-5_4-2016q3-20160926-win32\bin\arm-none-eabi-gcc.exe"
+LD	= "E:\Users\stm\local\gcc-arm-none-eabi-5_4-2016q3-20160926-win32\bin\arm-none-eabi-ld.exe"
+AR	= "E:\Users\stm\local\gcc-arm-none-eabi-5_4-2016q3-20160926-win32\bin\arm-none-eabi-ar.exe"
+OBJDUMP = "E:\Users\stm\local\gcc-arm-none-eabi-5_4-2016q3-20160926-win32\bin\arm-none-eabi-objdump.exe"
+CPPFILT = "E:\Users\stm\local\gcc-arm-none-eabi-5_4-2016q3-20160926-win32\bin\arm-none-eabi-c++filt.exe"
+OBJCOPY = "E:\Users\stm\local\gcc-arm-none-eabi-5_4-2016q3-20160926-win32\bin\arm-none-eabi-objcopy.exe"
